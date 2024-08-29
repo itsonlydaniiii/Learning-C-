49a7ce54c685a5f712ff687a22546007b49fccf5
Dani <174685774+itsonlydaniiii@users.noreply.github.com>
2024-08-29 16:20:51 +0200

    Add new learning material - Data types & Variables

--- learn-C#.cs
+++ learn-C#.cs
@@ -24,5 +24,51 @@ ComplicatedRainCalculationForTomorrow();
 // .NET generally refers to the family of programs and commands that let you make applications with C#
 
 
+//Data Types and Variables
+//Data Types
+/*  int - whole numbers, like: 1, -56, 948
+    double - decimal numbers, like: 239.43909, -660.01
+    char - single characters, like: “a”, “&”, “£”
+    string - string of characters, like: “dog”, “hello world”
+    bool - boolean values, like: true or false  */
+
+//Creating Variables with Types
+// Declare an integer
+int myAge;
+myAge = 32;
+// Declare a string
+string countryName = "Netherlands";
+
+/* Once we’ve defined a variable, we can use them throughout our program. For example, here’s a short program that
+prints a few  math equations to the console:    */
+int evenNumber = 22;
+int oddNumber = 45;
+Console.WriteLine(evenNumber + oddNumber); // Prints 67
+Console.WriteLine(oddNumber - evenNumber); // Prints 23
+
+//Using multiple Variable sets
+using System;
+
+namespace Form
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Create Variables
+string name = "Shadow";
+string breed = "Golden Retriever";
+int age = 5;
+double weight = 65.22;
+bool spayed = true;
+      // Print variables to the console
+Console.WriteLine(name);
+Console.WriteLine(breed);
+Console.WriteLine(age);
+Console.WriteLine(weight);
+Console.WriteLine(spayed);
+    }
+  }
+}
 
 
