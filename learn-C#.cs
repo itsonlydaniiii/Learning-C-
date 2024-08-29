3bb776acc7e89cb7522397ee798d24dd02451168
Dani <174685774+itsonlydaniiii@users.noreply.github.com>
2024-08-29 16:00:30 +0200

    Expose to firts C# material

---
+++ learn-C#.cs
@@ -0,0 +1,28 @@
+//Making comments in C#
+/*Multi-line comments
+are made like this in C# */
+
+
+//How to print basic text
+Console.WriteLine("Hello World!");
+
+
+//Getting Input
+Console.WriteLine("How old are you?");
+string input = Console.ReadLine();
+Console.WriteLine($"You are {input} years old!");
+// The code is ran in bash and is triggered by "$ dotnet run"
+
+
+/* This variable will be used to count the number of times anyone tweets the word persnickety */
+int persnicketyCount = 0;
+/* Calculates tomorrow's rain likelihood as a number between 0 and 100 */
+ComplicatedRainCalculationForTomorrow();
+
+//C# and it's alternatives like .NET allow more input per second from users - making busy websites faster than with JS
+/* C# is used to make interactive websites, mobile apps, video games, augmented and virtual reality (AR and VR), back-end services, and desktop applications */
+// .NET generally refers to the family of programs and commands that let you make applications with C#
+
+
+
+
